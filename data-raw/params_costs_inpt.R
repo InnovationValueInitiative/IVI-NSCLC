5340f2ede92a83bf8bc51b4b1febe211ce19e8e2
Incerti <devin.incerti@gmail.com>
2018-11-19 18:19:43 -0800

    Update inpatient and outpatient parameter estimates

--- data-raw/params_costs_inpt.R
+++ data-raw/params_costs_inpt.R
@@ -1,16 +1,6 @@
 rm(list = ls())
 library("data.table")
-library("iviNSCLC")
+library("readxl")
 
-# Outpatient costs by health state
-state_name <- iviNSCLC:::pkg_env$state_names_start1L_4[1:3]
-mean <- c(30000, 20000, 22000)
-se <- c(3000, 4000, 4200)
-ref <- c(NA, NA, NA)
-
-# Save
-params_costs_inpt <- data.table(state_name = state_name,
-                              mean = mean,
-                              se = se,
-                              ref = ref)
+params_costs_inpt <- data.table(read_excel("costs_inpt.xlsx", sheet = "costs_inpt"))
 save(params_costs_inpt, file = "../data/params_costs_inpt.rda", compress = "bzip2")
--- data-raw/params_costs_op.R
+++ data-raw/params_costs_op.R
@@ -1,16 +1,7 @@
 rm(list = ls())
 library("data.table")
-library("iviNSCLC")
+library("readxl")
 
-# Outpatient costs by health state
-state_name <- iviNSCLC:::pkg_env$state_names_start1L_4[1:3]
-mean <- c(1500, 2000, 2200)
-se <- c(300, 400, 420)
-ref <- c(NA, NA, NA)
-
-# Save
-params_costs_op <- data.table(state_name = state_name,
-                              mean = mean,
-                              se = se,
-                              ref = ref)
+params_costs_op <- data.table(read_excel("costs_op.xlsx", sheet = "costs_op"))
 save(params_costs_op, file = "../data/params_costs_op.rda", compress = "bzip2")
+
